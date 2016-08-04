962f6e9c7c6bd6abe30e0ffb9f9476891c1dea6e
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-08-04 16:00:41 -0300

    module to build html. first try.

---
+++ htmltags.py
@@ -0,0 +1,17 @@
+# class HtmlParts:
+#    def __init__(self):
+#        self.name = "py movie info"
+
+def top_header():
+    name = "py movie info"
+    print("<html>")
+    print("<header>")
+    print("<title>{}</title>" .format(name))
+    print("</header>")
+    print("<body>")
+
+def bottom_tags():
+    print("</body>")
+    print("</html>")
+
+
