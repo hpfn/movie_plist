51ddadb3c589cc9401225e12f31e5d20db216dfa
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-08-04 17:58:28 -0300

    hoppefully good format

--- htmltags.py
+++ htmltags.py
@@ -9,8 +9,12 @@ def top_header():
     print("<title>{}</title>" .format(name))
     print("</header>")
     print("<body>")
+    print("<table border=\"1\" width=\"100%\" cellpadding=\"4\" cellspacing=\"0\">")
+    print("<col width=\"50*\">")
+    print("<col width=\"300*\">")
 
 def bottom_tags():
+    print("</table>")
     print("</body>")
     print("</html>")
 
