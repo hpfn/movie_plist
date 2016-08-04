1e6eaeafb1a77a2618814ec3d5ba2c21379a35cf
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-08-04 14:35:13 -0300

    module pyscan: only scan a default directory

---
+++ pyscan.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import os
+
+
+def dir_to_scan():
+    """ return urls from .desktop files """
+    scan_dir = "/home/zaza/Vídeos/"
+    urls_movies = list()
+
+    for root, dir_name, filename in os.walk(scan_dir):
+        for wanted_file in filename:
+            if wanted_file.endswith('.desktop'):
+                file_to_search = root + '/' + wanted_file
+                with open(file_to_search, 'r') as check_content:
+                    url = check_content.readlines()
+                    if url:
+                        urls_movies.append(url[-2])
+
+    return urls_movies
