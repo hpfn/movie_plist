3222b8bbfb95864114ab35decf8ccbd7ca095f2b
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-08-06 17:42:16 -0300

    return a link to the directory with the movie

--- pyscan.py
+++ pyscan.py
@@ -4,7 +4,10 @@ import os
 
 
 def dir_to_scan():
-    """ return urls from .desktop files """
+    """
+       return urls from .desktop files and
+       path to dir with movie
+    """
     scan_dir = "/home/zaza/Vídeos/"
     urls_movies = list()
 
@@ -14,6 +17,6 @@ def dir_to_scan():
                 file_to_search = root + '/' + wanted_file
                 with open(file_to_search, 'r') as check_content:
                     url = check_content.readlines()
-                    urls_movies.append(url[-2].strip()[4:])
+                    urls_movies.append([url[-2].strip()[4:], root])
 
     return urls_movies
