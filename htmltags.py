3322bded77d01c872b39ee9d74cffd17e48bbb0d
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-08-06 11:21:19 -0300

    inside_tabel method is being written

--- htmltags.py
+++ htmltags.py
@@ -12,6 +12,33 @@ def top_header():
     print("<body>")
     print("<table border=\"1\" width=\"100%\" cellpadding=\"4\" cellspacing=\"0\">")
 
+def inside_tabel(poster_jpg, movie_data, link):
+    """
+       poster_jpg: jpg file
+       movie_data: list() with title, titleYear, director, writers, actors, synopsis
+       link: link to the directory where the movie is stored
+    """
+    print("<tr valign=\"top\">")
+
+    print("<td>")
+    poster_jpg
+    print("</td>")
+
+    print("<td>")
+    print("<p>")
+    movie.title()
+    movie.rate_value_and_votes()
+    movie.director()
+    movie.creator_writers()
+    movie.actors()
+    movie.synopsis()
+    link
+    print("</p>")
+    print("</td>")
+
+    print("</tr>")
+
+
 def bottom_tags():
     print("</table>")
     print("</body>")
