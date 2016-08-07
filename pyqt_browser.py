d56d157df663cc31a4ea15959f65654857f18bec
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-08-07 17:04:28 -0300

    trying to use QWebView.

--- pyqt_browser.py
+++ pyqt_browser.py
@@ -4,24 +4,29 @@
    this will be the first gui.
    only a browser.
 """
-from PyQt4.QtCore import * 
-from PyQt4.QtGui import * 
-
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 import sys
+from PyQt5.QtWebKit import *
+from PyQt5.QtWebKitWidgets import *
+
 
 def main():
-    app = QApplication (sys.argv)
-    #scene = QGraphicsScene()
-    #view = QGraphicsView()
-    #grid = QGridLayout()
-    browser = QTextBrowser()
+    app = QApplication(sys.argv)
+    # scene = QGraphicsScene()
+    # view = QGraphicsView()
+    # grid = QGridLayout()
+    browser = QWebView()  # QTextBrowser()
 
-    browser.setFixedSize(700,500)
-    browser.setSource(QUrl("pymovieinfo.html"))
-    browser.setWindowTitle("QTextBrowser HTML File Input")
+    browser.setFixedSize(700, 600)
+    # browser.setContent(mimeType='text/html')
+    browser.page().setLinkDelegationPolicy(QWebPage.DelegateAllLinks)
+    # browser.page
+    # browser.setSource(QUrl("pymovieinfo.html"))
+    browser.setUrl(QUrl.fromLocalFile('/home/zaza/Documentos/Programacao/python/ret_movie_info/pymovieinfo.html'))
+    browser.setWindowTitle('QTextBrowser HTML File Input')
     browser.show()
 
-    
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
