96e3ed30fa83c6d929580aef5f7c0e2944970e1e
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-08-09 11:32:18 -0300

    movie file added

--- first_use_urlib.py
+++ first_use_urlib.py
@@ -8,7 +8,7 @@ from htmltags import HtmlTags
 obtain_url = pyscan.dir_to_scan()
 html_page = HtmlTags()
 html_page.top_header()
-for url, path in obtain_url:
+for url, path, moviefile in obtain_url:
     m_data = list()
     html = urllib.request.urlopen(url).read()
     movie = pimdbdata.ParseImdbData(html)
@@ -25,6 +25,6 @@ for url, path in obtain_url:
 
     for i in [title_year, rate_votes, director, writers_list, actors_list, snps_txt]:
         m_data.append(i)
-    html_page.inside_table(m_poster, m_data, path)
+    html_page.inside_table(m_poster, m_data, path, moviefile)
 
 html_page.bottom_tags()
