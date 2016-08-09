d1f0ed3b9f7a61923845041d8e9159ba75640819
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-08-09 12:12:21 -0300

    complete rewrite to get the movie file. Avoid sample file - lower case only.

--- pyscan.py
+++ pyscan.py
@@ -13,13 +13,23 @@ def dir_to_scan():
 
     for root, dir_name, filename in os.walk(scan_dir):
         for wanted_file in filename:
+            urls_movies_stuff = list()
             if wanted_file.endswith('.desktop'):
                 file_to_search = root + '/' + wanted_file
-                movie_file = ''
-                if wanted_file.endswith( ('.avi', 'mp4', '.mkv')):
-                    movie_file = wanted_file
                 with open(file_to_search, 'r') as check_content:
                     url = check_content.readlines()
-                    urls_movies.append([url[-2].strip()[4:], root, movie_file])
+                    urls_movies_stuff.append(url[-2].strip()[4:])
+                    urls_movies_stuff.append(root)
+                    for path, dir_n, file_n in os.walk(root):
+                        for i in file_n:
+                            name = i.startswith('sample')
+                            if i.endswith(('.avi', '.mp4', '.mkv')) and not name:
+                                urls_movies_stuff.append(i)
+            # with open(file_to_search, 'r') as check_content:
+            # url = check_content.readlines()
+            # urls_movies.append([url[-2].strip()[4:], root, movie_file])
+            if urls_movies_stuff:
+                urls_movies.append(urls_movies_stuff)
 
+    print(urls_movies)
     return urls_movies
