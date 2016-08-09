d78d40430498d9a3f28242721cd773d9a132df9e
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-08-09 11:31:04 -0300

    movie file added

--- pyscan.py
+++ pyscan.py
@@ -15,8 +15,10 @@ def dir_to_scan():
         for wanted_file in filename:
             if wanted_file.endswith('.desktop'):
                 file_to_search = root + '/' + wanted_file
+                if wanted_file.endswith( ('.avi', 'mp4', '.mkv')):
+                    movie_file = wanted_file
                 with open(file_to_search, 'r') as check_content:
                     url = check_content.readlines()
-                    urls_movies.append([url[-2].strip()[4:], root])
+                    urls_movies.append([url[-2].strip()[4:], root, movie_file])
 
     return urls_movies
