ffb001c5205868f80a4946596e169706b0aa5954
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-08-20 11:21:01 -0300

    try/except for __init__ and insert_data

--- movie_plist_sqlite3.py
+++ movie_plist_sqlite3.py
@@ -1,19 +1,39 @@
 import sqlite3
+import sys
+
 
 # http://stackoverflow.com/questions/21360271/pythons-sqlite3-module-exceptions-where-is-the-documentation
+# https://www.python.org/dev/peps/pep-0249/#exceptions
+# http://pythoncentral.io/introduction-to-sqlite-in-python/
+# http://zetcode.com/db/sqlitepythontutorial/
 class DataStorage:
     def __init__(self):
-        self.conn = sqlite3.connect('movie_plist_sqlite3.db')
-        self.c = self.conn.cursor()
-        self.c.execute('''create table if not exists movie_plist (url UNIQUE,
-                        title_year TEXT, director, writers_list, actors_list,
-                        snps_txt TEXT, path, moviefile, watch INTEGER)''')
-        self.conn.commit()
+        try:
+            self.conn = sqlite3.connect('movie_plist_sqlite3.db')
+            self.c = self.conn.cursor()
+            self.c.execute('''create table if not exists movie_plist (url UNIQUE,
+                           title_year TEXT, director, writers_list, actors_list,
+                           snps_txt TEXT, path, moviefile, watch INTEGER)''')
+            self.conn.commit()
+        except sqlite3.Error as err:
+            if self.conn:
+                self.conn.rollback()
+            raise err
+            sys.exit(1)
+
+        finally:
+            self.conn.close()
 
     def insert_data(self, data_s):
         """ data_s will have seven itens """
-        self.c.execute('insert into movie_plist values (?,?,?,?,?,?,?,?,?)', data_s)
-        self.conn.commit()
+        try:
+            with self.conn:
+                self.conn.execute('insert into movie_plist values (?,?,?,?,?,?,?,?,?)', data_s)
+                self.conn.commit()
+        except sqlite3.IntegrityError:
+            if self.conn:
+                self.conn.rollback()
+            print("Record already exists")
 
     def show_data(self):
         self.c.execute('select * from movie_plist')
