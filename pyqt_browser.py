bd61c16798e598bfe375f97b36d42a8a7cbd0ded
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-09-07 12:11:17 -0300

    call vlc with mouse event. QAction

--- pyqt_browser.py
+++ pyqt_browser.py
@@ -9,6 +9,10 @@ from PyQt5.QtWidgets import *
 import sys
 from PyQt5.QtWebKit import *
 from PyQt5.QtWebKitWidgets import *
+from subprocess import call
+
+def call_vlc():
+    call('/usr/bin/vlc')
 
 
 def qt_browser(path_to_file):
@@ -17,6 +21,13 @@ def qt_browser(path_to_file):
     # view = QGraphicsView()
     # grid = QGridLayout()
     browser = QWebView()  # QTextBrowser()
+    browser.setContextMenuPolicy(Qt.ActionsContextMenu)
+    quitAction = QAction("VLC", None)
+    quitAction.triggered.connect(call_vlc)  # funcionando !!!
+    # funciona quitAction.triggered.connect(qApp.quit)
+    browser.addAction(quitAction)
+#    browser.setContextMenuPolicy(Qt.CustomContextMenu)
+#    browser.customContextMenuRequested.connect(openMenu)
 
     # browser.setFixedSize(700, 600)
     # browser.setContent(mimeType='text/html')
