5a9e3b1dd9097bd87259b3522ad0df5582f8faad
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-10-03 11:52:44 -0300

    reorganize module. two functions created. Class ?

--- pyscan.py
+++ pyscan.py
@@ -2,39 +2,45 @@
 
 import os
 
+def find_movie_file(urls_m_stuff2, file_name):
+    """ find the file to play """
+    for file_n in file_name:  # path, dir_n, file_n in os.walk(root):
+        # for i in file_n:
+        name = file_n.lower().startswith('sample')
+        if file_n.endswith(('.avi', '.mp4', '.mkv')) and not name:
+            urls_m_stuff2.append(file_n)
+
+    if len(urls_m_stuff2) < 3:
+        urls_m_stuff2.append("No_movie_file_yet")
+
+def open_right_file(urls_m_stuff, root_path, right_file):
+    """ open the right file and get the url"""
+    # urls_movies_stuff = list()
+    file_to_search = root_path + '/' + right_file
+    with open(file_to_search, 'r') as check_content:
+        url = check_content.readlines()
+        urls_m_stuff.append(url[-2][4:-1])
+        urls_m_stuff.append(root_path)
 
 def dir_to_scan(scan_dir):
     """
-       return urls from .desktop files and
-       path to dir with movie
-       movie file ( lower case ): avi, mp4, mkv
+       find .desktop file and call others functions
+       to append: path to dir with movie
+                  movie file ( lower case ): avi, mp4, mkv
     """
-    # scan_dir = "/path/to/dir/"
     urls_movies = list()
 
     for root, dir_name, filename in os.walk(scan_dir):
         for wanted_file in filename:
             urls_movies_stuff = list()
             if wanted_file.endswith('.desktop'):
-                file_to_search = root + '/' + wanted_file
-                with open(file_to_search, 'r') as check_content:
-                    url = check_content.readlines()
-                    urls_movies_stuff.append(url[-2][4:-1])
-                    urls_movies_stuff.append(root)
-                    for file_n in filename:  # path, dir_n, file_n in os.walk(root):
-                        # for i in file_n:
-                        name = file_n.lower().startswith('sample')
-                        if file_n.endswith(('.avi', '.mp4', '.mkv')) and not name:
-                            urls_movies_stuff.append(file_n)
-            # with open(file_to_search, 'r') as check_content:
-            # url = check_content.readlines()
-            # urls_movies.append([url[-2].strip()[4:], root, movie_file])
+                # urls_movies_stuff = list()
+                open_right_file(urls_movies_stuff, root, wanted_file)
+                find_movie_file(urls_movies_stuff, filename)
+
             if urls_movies_stuff:
-                if len(urls_movies_stuff) < 3:
-                    urls_movies_stuff.append("No_movie_file_yet")
                 urls_movies.append(urls_movies_stuff)
 
-    # print(urls_movies)
     return urls_movies
 
-# dir_to_scan("/home/zaza/Vídeos")
+
