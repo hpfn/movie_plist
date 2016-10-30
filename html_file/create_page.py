0b1bf9240edba5193cc9834d90649493ed1f3b1f
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-10-30 17:56:38 -0200

    param name change

--- html_file/create_page.py
+++ html_file/create_page.py
@@ -1,7 +1,7 @@
 from html_file.htmltags import HtmlTags
 
 
-def generate_html(d_scan, stored_data):
+def generate_html(d_scan, stored_d):
     """ create the html file:
           - put the first tags
           - get the info from the db
@@ -9,6 +9,6 @@ def generate_html(d_scan, stored_data):
     """
     html_page = HtmlTags(d_scan)
     html_page.top_header()
-    m_data = stored_data.show_data()
+    m_data = stored_d.show_data()
     html_page.parse_data_to_inside_table(m_data)
     html_page.bottom_tags()
