600f37a21265ebadade8cfc257024591ee8670b7
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-11-06 12:24:03 -0200

    create .html or insert movie entry in it directly. less db query.

--- html_file/create_page.py
+++ html_file/create_page.py
@@ -1,14 +1,25 @@
 from html_file.htmltags import HtmlTags
+from pathlib import Path
 
-
-def generate_html(d_scan, stored_d):
-    """ create the html file:
+def generate_html(d_scan, data_from_db):
+    """
+        create the html file:
           - put the first tags
           - get the info from the db
           - put final tags
+
+        or
+
+        append to .html file a new movie after
+        seek(195) - after the top_header method.
     """
-    html_page = HtmlTags(d_scan, 'w')
-    html_page.top_header()
-    # m_data = stored_d.show_data()
-    html_page.parse_data_to_inside_table(stored_d.show_data())
-    html_page.bottom_tags()
+    movie_file_check = d_scan + "/pymovieinfo.html"
+    movie_file_check = Path(movie_file_check)
+    if not movie_file_check.is_file():
+        html_page = HtmlTags(d_scan, 'w')
+        html_page.top_header()
+        html_page.parse_data_to_inside_table(data_from_db)
+        html_page.bottom_tags()
+    else:
+        HtmlTags(d_scan, 'inplace', data_from_db)
+
--- movie_plist.py
+++ movie_plist.py
@@ -3,50 +3,42 @@
 import sys
 import time
 from PyQt5.QtWidgets import QApplication
-from pathlib import Path
 # movie_plist stuff
 import html_file.create_page
 from data.pyscan import PyScan
 from info_in_db.movie_plist_sqlite3 import DataStorage
 from pyqt_gui.main_gui import Window
 
-def check_pushto_db(stored, url_got):
-    """ if movie info is not in db
-        put data in it
+def check_pushto_db(url_got, scan):
     """
-    movies_stored = stored.check_movie()
+        if movie info is not in db put data in it
+        also call create_page (html) module
+    """
+    data_to_html = list()
+    stored_data = DataStorage()
+    movies_stored = stored_data.check_movie()
     # check if the movie info is in movie_plist_sqlite3.db
     # if not, put it in there
     for url, path, movie_file in url_got:
         if url not in movies_stored:
-        #if not stored.check_movie(url):
-            stored.insert_data(url, path, movie_file)
+            data_to_html.append(stored_data.insert_data(url, path, movie_file))
+
+    stored_data.exit_from_db()
+
+    if data_to_html:
+        html_file.create_page.generate_html(scan, data_to_html)
+
 
 def main(d_scan):
     # scan the directory
     obtain_url = PyScan(d_scan)
     obtain_url = obtain_url.dir_to_scan()
-    # check if the .html exists
-    movie_file_check = d_scan + "/pymovieinfo.html"
-    movie_file_check = Path(movie_file_check)
-    # open connection with *sqlite3.db
-    stored_data = DataStorage()
 
-    if movie_file_check.is_file():
-        # .html file exists. Do nothing.
-        pass
-    else:
-        check_pushto_db(stored_data, obtain_url)
-        # create the page for QWebView
-        # here is the performance problem
-        start = time.time()
-        html_file.create_page.generate_html(d_scan, stored_data)
-        end = time.time()
-        print(end - start)
+    # will push data to db if necessary
+    # and call create_page.generate_html
+    check_pushto_db(obtain_url, d_scan)
 
-    # close connection with *sqlite3.db
-    stored_data.exit_from_db()
-
+    # launch movie_plist
     app = QApplication(sys.argv)
     ex = Window(d_scan)
     sys.exit(app.exec_())
