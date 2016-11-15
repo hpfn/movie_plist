ee00abe7f83d9c516e8624b54913ebd15d9f8db1
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-11-15 12:14:08 -0200

    update db and edit .html file

--- cgi-bin/mark_seen.py
+++ cgi-bin/mark_seen.py
@@ -1,14 +1,82 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
+
+# how to pass the path to .html file
+# how to update the seen list on combobox
+
+from info_in_db.movie_plist_sqlite3 import DataStorage
+import time
 import cgi
 import cgitb
+
 cgitb.enable()
 
+
 def start_response(resp="text/html"):
-    return('Content-type: ' + resp + '\n\n')
+    return 'Content-type: ' + resp + '\n\n'
+
+
+def head():
+    html_to_body = ("<html>\n<head>\n"
+                    "<meta http-equiv=\"Content-Type\" content=\"text/html_file; charset=utf-8\">\n"
+                    "<title>Py Movie Info</title>\n"
+                    "</head>\n<body>\n")
+    return html_to_body
+
+
+def footer():
+    return "<p>Please, wait a few seconds, right click on mouse and 'Go Back'</p></body>\n</html>"
+
+
+def edit_html(movie_title):
+    start_line = ''
+    end_line = ''
+    count = 1
+    with open('/home/zaza/Vídeos/index.html', 'r') as html_file:
+        html_file_lines = html_file.readlines()
+
+    mark_start = 'start ' + movie_title
+    mark_end = 'end ' + movie_title
+    for unwanted in html_file_lines:
+        if mark_start in unwanted:
+            start_line = count
+        if mark_end in unwanted:
+            end_line = count
+        count += 1
+
+    print("remove from {} to {}" .format(start_line, end_line))
+    del html_file_lines[start_line:end_line+1]
+
+    f = open('/home/zaza/Vídeos/index.html', 'w')
+    f.writelines(html_file_lines)
+    f.close()
+
 
 form_data = cgi.FieldStorage()
 movie = form_data.getvalue("title_year")
+stored_data = DataStorage()
 
 print(start_response())
-print("{}" .format(movie))
+print('')
+print(head())
+print('')
+if isinstance(movie, list):
+    for i in movie:
+        print('<p>')
+        print("Marking {} as Seen on db...".format(i))
+        print('</p>')
+        stored_data.update_movie_watch('1', i)
+        time.sleep(2)
+        edit_html(i)
+        time.sleep(2)
+
+else:
+    print('<p>')
+    print("Marking {} as Seen on db...".format(movie))
+    print('</p>')
+    stored_data.update_movie_watch('1', movie)
+    time.sleep(2)
+    edit_html(movie)
+    time.sleep(2)
+print(footer())
