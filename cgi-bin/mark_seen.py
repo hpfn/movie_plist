2a2f51da9bc4b045ab5b30d84c48a0dd1c267d17
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-11-19 10:34:25 -0200

    edit_html() in a module

--- cgi-bin/mark_seen.py
+++ cgi-bin/mark_seen.py
@@ -5,10 +5,10 @@
 # how to update the seen list on combobox
 # with a module ? var at least
 from info_in_db.movie_plist_sqlite3 import DataStorage
+from html_file.remove_movie import EditHtml
 # import time
 import cgi
 import cgitb
-
 cgitb.enable()
 
 
@@ -28,31 +28,6 @@ def head():
 def footer():
     return "<p>Please, wait a few seconds...</p></body>\n</html>"
 
-
-def edit_html(movie_title):
-    start_line = ''
-    end_line = ''
-    count = 0
-    with open('/home/zaza/Vídeos/index.html', 'r') as html_file:
-        html_file_lines = html_file.readlines()
-
-    mark_start = 'start ' + movie_title
-    mark_end = 'end ' + movie_title
-    for unwanted in html_file_lines:
-        if mark_start in unwanted:
-            start_line = count
-        if mark_end in unwanted:
-            end_line = count
-        count += 1
-
-    print("remove from {} to {}" .format(start_line, end_line))
-    del html_file_lines[start_line:end_line+1]
-
-    f = open('/home/zaza/Vídeos/index.html', 'w')
-    f.writelines(html_file_lines)
-    f.close()
-
-
 form_data = cgi.FieldStorage()
 movie = form_data.getvalue("title_year")
 stored_data = DataStorage()
@@ -67,11 +42,11 @@ if isinstance(movie, list):
         print("Marking {} as Seen on db...".format(i))
         print('</p>')
         stored_data.update_movie_watch('1', i)
-        edit_html(i)
+        EditHtml(i)
 else:
     print('<p>')
     print("Marking {} as Seen on db...".format(movie))
     print('</p>')
     stored_data.update_movie_watch('1', movie)
-    edit_html(movie)
+    EditHtml(movie)
 print(footer())
