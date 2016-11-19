b132f0367751dc2c785bcbac792dfe02257be794
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-11-19 09:48:37 -0200

    relative import

--- pyqt_gui/main_gui.py
+++ pyqt_gui/main_gui.py
@@ -1,6 +1,6 @@
 
-from pyqt_gui.pyqt_browser import qt_browser
-from pyqt_gui.combo_box_build import Combo
+from .pyqt_browser import qt_browser
+from .combo_box_build import Combo
 
 from PyQt5.QtWidgets import QGridLayout
 from PyQt5.QtWidgets import QWidget  # , QRadioButton
