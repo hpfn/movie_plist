3a93fe2576b23659e383ead542547e97037dc37e
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-11-29 15:45:39 -0200

    try/except on confirm_option method.

--- pyqt_gui/combo_box_seen.py
+++ pyqt_gui/combo_box_seen.py
@@ -21,24 +21,22 @@ class CboxSeen(Combo):
         self.show_list()
 
     def confirm_option(self, index, movie_selected):
-        """ show msg about what to_do with the movie selected"""
-        txt_info = "You will watch again " + movie_selected
-        msg = QMessageBox()
-        reply = msg.question(self, 'The selected movie', txt_info,
-                             QMessageBox.Yes | QMessageBox.Cancel, QMessageBox.Cancel)
-
-        if reply == QMessageBox.Yes:
-            msg.setText('remove this!!!')
-
-            html_cboxlist_changes = InteractBox(movie_selected)
-            html_cboxlist_changes.watch_movie()
-        else:
-            # how to ignore this ???
-            # QEvent.setAccepted()
-            msg.setText('Doing nothing.')
-
-        msg.show()
-        msg.exec_()
+        """ show msg with a synopsis of the movie"""
+        # msg.setIcon(QMessageBox.Information)
+        try:
+            txt_info = "Put a synopsis here for " + movie_selected
+            msg = QMessageBox()
+            reply = msg.question(self, 'The selected movie', txt_info,
+                                 QMessageBox.Yes | QMessageBox.Cancel, QMessageBox.Cancel)
+            if reply == QMessageBox.Yes:
+                html_cboxlist_changes = InteractBox(movie_selected)
+                html_cboxlist_changes.watch_movie()
+            else:
+                msg.text("Ignore this window")
+            msg.exec()
+        except TypeError:
+            print('Error when trying to load the movie')
+            print('"{}" is not a valid entry' .format(movie_selected))
 
 
 
