25e63c710bd0795eda5fdd33e4d25d59a45a22f7
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-12-02 16:19:43 -0200

    one 'if' instead of try/except.

--- pyqt_gui/combo_box_seen.py
+++ pyqt_gui/combo_box_seen.py
@@ -25,20 +25,20 @@ class CboxSeen(Combo):
     def confirm_option(self, index, movie_selected):
         """ show msg with a synopsis of the movie"""
         # msg.setIcon(QMessageBox.Information)
-        try:
+        if index:
             txt_info = self.stored_data.movie_synopsis(movie_selected)
             msg = QMessageBox()
             reply = msg.question(self, movie_selected + ' synopsis: ', txt_info[0],
                                  QMessageBox.Yes | QMessageBox.Cancel, QMessageBox.Cancel)
+
             if reply == QMessageBox.Yes:
+                msg.setText('useless')
                 html_cboxlist_changes = InteractBox(movie_selected)
                 html_cboxlist_changes.watch_movie()
             else:
-                msg.text("Ignore this window")
+                msg.setText('nothing')
+
             msg.exec()
-        except TypeError:
-            print('Error when trying to load the movie')
-            print('"{}" is not a valid entry' .format(movie_selected))
 
 
 
