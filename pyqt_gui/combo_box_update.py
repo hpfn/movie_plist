c7279977d352e80695a7544b19cbdd720e62753f
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-12-02 10:02:37 -0200

    rm commented lines. commented lines about QMessageBox

--- pyqt_gui/combo_box_update.py
+++ pyqt_gui/combo_box_update.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMessageBox
+# from PyQt5.QtWidgets import QMessageBox
 # from PyQt5.QtCore import QEvent
 from .combo_box_build import Combo
 from .combo_box_interact import InteractBox
@@ -16,29 +16,24 @@ class CboxUpdate(Combo):
             and be easier to find the index on
             confirm_option method (InteractBox())
         """
-        # self.movies_stored = ['insert movie file']
         self.movies_stored = [i[0] for i in self.stored_data.no_movie_yet()]
         self.movies_stored.insert(0, 'insert movie file')
-        #for i in self.stored_data.no_movie_yet():
-        #    self.movies_stored.append(i[0])
-
-        # self.show_list()
 
     def confirm_option(self, index, movie_selected):
         """ show msg about what to_do with the movie selected"""
-        msg = QMessageBox()
+        # msg = QMessageBox()
         try:
             html_cboxlist_changes = InteractBox(movie_selected)
             html_cboxlist_changes.insert_movie_file_action(self.browser_reload)
             self.removeItem(index)
-            txt_info = movie_selected + " movie file on the .html file"
-            msg.setText(txt_info)
+            # txt_info = movie_selected + " movie file on the .html file"
+            # msg.setText(txt_info)
         except TypeError:
             print('Error when updating the .html file')
             print('"{}" is not a valid entry' .format(movie_selected))
             # how to ignore this ???
             # QEvent.setAccepted()
-            msg.setText('Doing nothing.')
+            # msg.setText('Doing nothing.')
 
-        msg.show()
-        msg.exec_()
+        # msg.show()
+        # msg.exec_()
