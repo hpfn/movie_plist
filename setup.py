fe20b2d80302eab5779f3dceac855617c6372a4f
Herbert Parentes Fortes Neto <hpfn@debian.org>
2016-12-25 10:55:45 -0200

    have to learn about setuptools, though.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='movie_plist',
+    version='20161225',
+    packages=['conf', 'data', 'pyqt_gui', 'html_file', 'info_in_db'],
+    url='https://github.com/hpfn/movie_plist',
+    license='GPL-3.0+',
+    author='zaza',
+    author_email='hpfn@users.noreply.github.com',
+    description='list ypur movies'
+)
