68ffe1adb8fe2704b74d01151fa6c3abc3af1c00
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-04-10 12:02:33 -0300

    fixing a wrong 'cp file file.repo'.

--- splitter.py
+++ splitter.py
@@ -13,12 +13,17 @@ from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QHBoxLayout, QLabel,
         QSplitter, QListWidget, QTabWidget, QFileSystemModel, QTreeView)
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-
+import urllib.request
+from data import pimdbdata
 
 class TwoLines(QWidget):
-    def __init__(self):
+    def __init__(self, first_list, unseen_d):
         super().__init__()
         self.top = QListWidget()
+        self.first_list = first_list
+        # self.seen_d = seen_d
+        # self.unseen_d = unseen_d
+        self. current_dict = unseen_d
         self.tabs = QTabWidget()
         # movie info
         self.tab_synopsys = QWidget()
@@ -27,7 +32,7 @@ class TwoLines(QWidget):
         # layout
         self.synopsys_vbox = QVBoxLayout()
         self.lsdir_vbox = QVBoxLayout()
-        # movie info 
+        # movie info
         self.bottom = QLabel()
         # ls content of the current dir
         self.lsdir = QFileSystemModel()
@@ -40,7 +45,7 @@ class TwoLines(QWidget):
 
         # will be the scan result (unseen)
         list_items = ['unseen 1', 'unseen 2', 'unseen 3']
-        self.top.addItems(list_items)
+        self.top.addItems(self.first_list)
         self.top.setCurrentRow(0)
         # self.top.itemClicked.connect(self.top.Clicked)
         #self.bottom.setText(self.top.currentItem().text())
@@ -88,23 +93,30 @@ class TwoLines(QWidget):
         self.tabs.addTab(self.tab_ls_dir, "ls dir")
 
     def data_to_show(self):
-        """ get data from a dict or named_tupla """
+        """ get data from a dict """
+        url = self.current_dict[self.top.currentItem().text()][0]
+        html = urllib.request.urlopen(url).read()
+        movie = pimdbdata.ParseImdbData(html)
+        poster = movie.movie_poster()
+        synopsis = movie.synopsis()
+        # print(poster)
         # if no internet, commented
-        #img = QImage()  # (8,10,4)
-        #data = urllib.request.urlopen(
+        img = QImage()  # (8,10,4)
+        data = urllib.request.urlopen(poster).read()
         #        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc5Mzg3NjI4OF5BMl5BanBnXkFtZTgwNzA3Mzg4MDI@._V1_UX182_CR0,0,182,268_AL_.jpg").read()
-        #img.loadFromData(data)
-        #img.save('picture.png')
-        texto = '<html><table><td><img src="picture.png"></td><td>' + self.top.currentItem().text() + '</td></table></html>'
+        img.loadFromData(data)
+        img.save('picture.png')
+        texto = '<html><table><td><img src="picture.png"></td><td>' + synopsis + '</td></table></html>'
         self.bottom.setText(texto)
 
     def ls_current_dir(self):
-        dir_to_path = "/tmp"
+        dir_to_path = self.current_dict[self.top.currentItem().text()][1]
+        # dir_to_path = "/tmp"
         self.lsdir.setRootPath(dir_to_path)
         self.tree.setModel(self.lsdir)
         self.tree.setRootIndex(self.lsdir.index(dir_to_path))
-        #self.tree.resizeColumnToContents(100)
-        self.tree.AdjustToContents = 2
+        # self.tree.resizeColumnToContents(100)
+        # self.tree.AdjustToContents = 2
         self.tree.setAnimated(True)
         self.tree.setIndentation(30)
         
