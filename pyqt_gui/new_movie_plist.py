659319746c74554a237f81bdff3a8d77f4b4752e
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-04-20 10:24:01 -0300

    pep8

--- pyqt_gui/new_movie_plist.py
+++ pyqt_gui/new_movie_plist.py
@@ -12,13 +12,13 @@ from pyqt_gui import splitter
 
 
 class Window(QMainWindow):
-    def __init__(self, s_list, us_list, all_movies): #m_seen, m_unseen):
+    def __init__(self, s_list, us_list, all_movies):  # m_seen, m_unseen):
         super().__init__()
         self.two_lines = splitter.TwoLines(s_list, us_list, all_movies)
         self.seen_list = s_list
         self.unseen_list = us_list
-        #self.seen_dict = m_seen
-        #self.unseen_dict = m_unseen
+        # self.seen_dict = m_seen
+        # self.unseen_dict = m_unseen
 
         self.init_ui()
 
@@ -75,9 +75,7 @@ class Window(QMainWindow):
             self.two_lines.top.setCurrentRow(0)
             # self.two_lines.current_dict = self.seen_dict
 
-
-#if __name__ == '__main__':
+# if __name__ == '__main__':
 #    app = QApplication(sys.argv)
 #    ex = Window()
 #    sys.exit(app.exec_())
-
