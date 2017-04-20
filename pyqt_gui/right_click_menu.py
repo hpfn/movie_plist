f71dbf02b3141666eed9d6257e8014fbe5ad40da
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-04-20 11:15:40 -0300

    right click menu owns a home.

---
+++ pyqt_gui/right_click_menu.py
@@ -0,0 +1,70 @@
+from PyQt5.QtWidgets import QMenu, QAction
+from PyQt5.QtGui import QCursor
+
+
+class RightClickMenu():
+    def __init__(self, current_list, current_dict, qt_list, s_list):
+        self.current_item = qt_list.currentItem().text()
+        self.current_list = current_list
+        self.current_dict = current_dict[self.current_item][0]
+        self.qt_list = qt_list
+        self.s_list = s_list
+        self.menu = QMenu()
+
+        self.right_click()
+
+    def right_click(self):
+        m_seen_action = QAction('Mark as Seen', self.menu)
+        # unseenAction.setShortcut()
+        m_seen_action.setStatusTip('Mark as Seen')
+        m_seen_action.triggered.connect(self.m_seen_movies)
+
+        m_rm_action = QAction('Remove from Database', self.menu)
+        # unseenAction.setShortcut()
+        m_rm_action.setStatusTip('Remove from Database')
+        m_rm_action.triggered.connect(self.m_rm_from_db)
+
+        self.menu.addAction(m_seen_action)
+        self.menu.addAction(m_rm_action)
+
+        self.menu.exec_(QCursor.pos())
+
+    def m_seen_movies(self):
+        """
+        mark a movie as seen. 
+        check unseen list and seen list
+        check on db if it is already a seen movie
+        """
+        from info_in_db.movie_plist_sqlite3 import DataStorage
+
+        title_year = self.current_item
+        url = self.current_dict
+        stored_data = DataStorage()
+
+        if stored_data.movie_isregistered(url):
+            pass
+        else:
+            self.current_list.remove(title_year)
+            self.qt_list.takeItem(self.qt_list.currentRow())
+            self.s_list.append(title_year)
+            stored_data.insert_data(url)
+
+    def m_rm_from_db(self):
+        """
+        remove from current list and from db
+        the user remove from HD
+        """
+        from PyQt5.QtWidgets import QMessageBox
+        from info_in_db.movie_plist_sqlite3 import DataStorage
+
+        title_year = self.current_item
+        url = self.current_dict
+        self.current_list.remove(title_year)
+        self.qt_list.takeItem(self.qt_list.currentRow())
+        stored_data = DataStorage()
+        stored_data.movie_delete(url)
+
+        msg = QMessageBox()
+        msg.setText(title_year + "\n removed from DB.\n Remove from HD yourself.")
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec()
