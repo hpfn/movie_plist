346f1fc17aafbeb0e858cdcb3b302e72f1935748
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-04-23 16:17:11 -0300

    update movie count. but only when clicking on a button.

--- pyqt_gui/main_window.py
+++ pyqt_gui/main_window.py
@@ -6,8 +6,6 @@ from zetcode tutorial
 """
 
 from PyQt5.QtWidgets import (QMainWindow, QAction)
-
-# from PyQt5.QtCore import QIcon
 from pyqt_gui import splitter
 
 
@@ -17,15 +15,10 @@ class Window(QMainWindow):
         self.two_lines = splitter.TwoLines(s_list, us_list, all_movies)
         self.seen_list = s_list
         self.unseen_list = us_list
-        # self.seen_dict = m_seen
-        # self.unseen_dict = m_unseen
 
         self.init_ui()
 
     def init_ui(self):
-        # textEdit = QTextEdit()
-        # self.setCentralWidget(textEdit)
-        # self.two_lines = splitter.TwoLines()
         self.setCentralWidget(self.two_lines)
 
         exit_action = QAction('Exit', self)
@@ -33,23 +26,18 @@ class Window(QMainWindow):
         exit_action.setStatusTip('Exit app')
         exit_action.triggered.connect(self.close)
 
-        count_unseen = str(len(self.unseen_list))
         unseen_action = QAction('Unseen', self)
         # unseenAction.setShortcut()
-        unseen_action.setStatusTip('Unseen movies: ' + count_unseen)
+        # unseen_action.setStatusTip('Unseen movies: ' + count_unseen)
         unseen_action.triggered.connect(self.unseenmovies)
 
-        count_seen = str(len(self.seen_list))
         seen_action = QAction('Seen', self)
         # unseenAction.setShortcut()
-        seen_action.setStatusTip('Seen movies: ' + count_seen)
+        # seen_action.setStatusTip('Seen movies: ' + count_seen)
         seen_action.triggered.connect(self.seenmovies)
 
-        self.statusBar()
-
-        # menubar = self.menuBar()
-        # fileMenu = menubar.addMenu('&File')
-        # fileMenu.addAction(exitAction)
+        # self.statusBar().showMessage('Unseen: ' + count_unseen + ' | Seen: ' + count_seen)
+        self.update_statusbar()
 
         toolbar = self.addToolBar('Exit')
         toolbar.addAction(exit_action)
@@ -66,7 +54,7 @@ class Window(QMainWindow):
         self.two_lines.current_list = self.unseen_list
         self.two_lines.top.addItems(self.unseen_list)
         self.two_lines.top.setCurrentRow(0)
-        # self.two_lines.current_dict = self.unseen_dict
+        self.update_statusbar()
 
     def seenmovies(self):
         # botão 'seen'
@@ -75,9 +63,9 @@ class Window(QMainWindow):
             self.two_lines.current_list = self.seen_list
             self.two_lines.top.addItems(self.seen_list)
             self.two_lines.top.setCurrentRow(0)
-            # self.two_lines.current_dict = self.seen_dict
+            self.update_statusbar()
 
-# if __name__ == '__main__':
-#    app = QApplication(sys.argv)
-#    ex = Window()
-#    sys.exit(app.exec_())
+    def update_statusbar(self):
+        self.statusBar().clearMessage()
+        self.statusBar().showMessage('Unseen: ' + str(len(self.unseen_list)) +
+                                     ' | Seen: ' + str(len(self.seen_list)))
