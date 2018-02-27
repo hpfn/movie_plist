c7897a552fa6926aa049ceb726f1c9d9498f61e3
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-02-27 09:13:03 -0300

    unseen_json_file

--- movie_plist.py
+++ movie_plist.py
@@ -2,7 +2,7 @@
 # -*-coding-utf8-*
 import sys, json
 from PyQt5.QtWidgets import QApplication
-from conf.global_conf import internet_on, get_dir_path, json_file
+from conf.global_conf import internet_on, get_dir_path, seen_json_file, unseen_json_file
 from data.pyscan import create_dicts
 from pyqt_gui.main_window import Window
 
@@ -23,15 +23,16 @@ def main(d_scan):
     app = QApplication(sys.argv)
     ex = Window(movie_seen, movie_unseen)
     
-    def json_seen_m(movie_dic):
-        #unseen_l = set(u_l)
-        # seen_dict = {key: list_items
-        #             for key, list_items in movie_dic.items()
-        #             if not key in u_l}
+    def w_json_movie_file(movie_dic, json_file):
         with open(json_file, 'w') as outfile:
             json.dump(movie_dic, outfile)
 
-    sys.exit([app.exec_(), json_seen_m(movie_seen)])
+    exit_task = [
+        w_json_movie_file(movie_unseen, unseen_json_file),
+        w_json_movie_file(movie_seen, seen_json_file),
+        app.exec_()
+    ]
+    sys.exit(exit_task)
 
 
 if __name__ == '__main__':
