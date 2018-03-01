18b5320b31579053e3d19c4d611fdd87a2510884
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-03-01 10:48:42 -0300

    Studying a less nested for loop

--- data/pyscan.py
+++ data/pyscan.py
@@ -4,6 +4,7 @@ import os
 import re
 import sys
 import json
+import time
 # from PyQt5.QtWidgets import QMessageBox, QApplication
 from conf.global_conf import seen_json_file, unseen_json_file
 
@@ -47,6 +48,16 @@ def dir_to_scan(scan_dir, seen_movies):
     arq_pattern = re.compile(r"[\w,'-.]+\.desktop")
     # dirname is the title_year if the user do it right
     dirname_titleyear = re.compile('/.*/')
+    # selection_dirs = ((root, filename)
+    #                   for root, _, filename in os.walk(scan_dir)
+    #                   if not {root}.issubset(seen_movies))
+    #
+    # for root, filename in selection_dirs:
+    #     this_one = re.search(arq_pattern, ' '.join(filename))
+    #     if this_one:
+    #         imdb_url = open_right_file(root, this_one.group(0))
+    #         yield dirname_titleyear.sub('', root), [imdb_url, root]
+
     for root, _, filename in os.walk(scan_dir):
         if not {root}.issubset(seen_movies):
             this_one = re.search(arq_pattern, ' '.join(filename))
@@ -61,6 +72,7 @@ def create_dicts(s_dir):
     # build unseen movies based on json file
     # if no unseen movies ask if continue
     """
+    start = time.time()
     with open(seen_json_file) as json_data:
         movie_seen = json.load(json_data)
 
@@ -76,5 +88,8 @@ def create_dicts(s_dir):
 
     if len(movie_unseen) == 0:
         empty_unseen_dict()
+
+    end = time.time()
+    print(end - start)
 
     return movie_seen, movie_unseen
