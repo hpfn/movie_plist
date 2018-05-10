cc6daaa1b340ed8f0c06e56035de9466788a3d0b
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-05-10 18:01:15 -0300

    File in a better format

--- tests/test_pyscan.py
+++ tests/test_pyscan.py
@@ -4,43 +4,64 @@ import os
 sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
 
 
-@pytest.fixture
-def py_scan():
-    from data import pyscan
+#@pytest.fixture
+#def py_scan():
+from data import pyscan
 
-    pyscan.seen_json_file = './tests/seen_json_file.json'
-    pyscan.unseen_json_file = './tests/unseen_json_file.json'
+pyscan.seen_json_file = './tests/seen_json_file.json'
+pyscan.unseen_json_file = './tests/unseen_json_file.json'
 
-    c_d = pyscan.CreateDict('./tests/videos_test/')
+c_d = pyscan.CreateDict('./tests/videos_test/')
+#    return c_d
+movie_seen, movie_unseen = c_d.create_dicts()
+url, path_to = list(movie_unseen.values())[0]
 
-    return c_d
-
-params = [
-
+instance_params = [
+    (isinstance(movie_unseen, dict), True),
+    (isinstance(movie_seen, dict), True)
 ]
 
 
-def test_create_dicts():
-    scan_dir = py_scan()
-
-    _, movie_unseen = scan_dir.create_dicts()
-    assert isinstance(movie_unseen, dict)
-    assert 'Shawshank Redemption, the 1994' in movie_unseen.keys()
-    assert len(movie_unseen) == 1
+@pytest.mark.parametrize("a,b", instance_params)
+def test_dict_instance(a, b):
+    assert a is b
 
 
-def test_unknow_dirs():
-    scan_dir = py_scan()
-
-    unkown_dirs = scan_dir._unknow_dirs()
-    list_scan_dir = list(unkown_dirs)
-    assert len(list_scan_dir) == 2
-    assert './tests/videos_test/Shawshank Redemption, the 1994' in list_scan_dir[1]
+str_params = [
+    ('Shawshank Redemption, the 1994', movie_unseen.keys()),
+    ('https://www.imdb.com/title/tt0111161/', url),
+    ('./tests/videos_test/Shawshank Redemption, the 1994', path_to)
+]
 
 
-def test_new_desktop_f():
-    scan_dir = py_scan()
+@pytest.mark.parametrize("a,b", str_params)
+def test_dict(a, b):
+    assert a in b
 
-    new_desktop_f = scan_dir._new_desktop_f()
-    new_desktop_f = list(new_desktop_f)
-    assert len(new_desktop_f) == 1
+
+size_params = [
+    (len(movie_unseen), 1),
+    (len(movie_seen), 0)
+]
+
+
+@pytest.mark.parametrize("a,b", size_params)
+def test_dict_size(a, b):
+    assert a == b
+
+
+# def test_unknow_dirs():
+#     scan_dir = py_scan()
+#
+#     unkown_dirs = scan_dir._unknow_dirs()
+#     list_scan_dir = list(unkown_dirs)
+#     assert len(list_scan_dir) == 2
+#     assert './tests/videos_test/Shawshank Redemption, the 1994' in list_scan_dir[1]
+#
+#
+# def test_new_desktop_f():
+#     scan_dir = py_scan()
+#
+#     new_desktop_f = scan_dir._new_desktop_f()
+#     new_desktop_f = list(new_desktop_f)
+#     assert len(new_desktop_f) == 1
