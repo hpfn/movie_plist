f21b580d4c1646c734987d9c19e4310eb7063daf
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-05-11 09:14:09 -0300

    Move methods from htmltags to pimdbdata

--- data/pimdbdata.py
+++ data/pimdbdata.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
-from bs4 import BeautifulSoup
 import re
+import urllib.request
+import urllib.error
+from _socket import timeout
+from PyQt5.QtGui import QImage
+from bs4 import BeautifulSoup
 
 
-class ParseImdbData(object):
-    def __init__(self, html):
-        """ html is the url to be parsed """
-        self.soup = BeautifulSoup(html, 'html.parser')
+class ParseImdbData:
+    def __init__(self, url):
+        """
+        receive an url to be
+        """
+        # self.url = url
+        html_page = self._get_html(url)
+        self.soup = BeautifulSoup(html_page, 'html.parser')
+        self._do_poster_png_file()
 
     def title_year(self):
         """
@@ -14,14 +23,55 @@ class ParseImdbData(object):
         """
         return self.soup.title.string[:-7]
 
-    def movie_poster(self):
+    def synopsis(self):
+        """
+        obs: tem um conserto no azak.
+        """
+        description = self.soup.find(itemprop="description")
+        raw_txt = description.get_text()
+        return raw_txt
+
+    def _do_poster_png_file(self):
+        """
+        obs: tem um conserto no azak
+        """
+        try:
+            poster = self._movie_poster()
+            data = urllib.request.urlopen(poster).read()
+            self._save_poster_file(data)
+        except urllib.error.URLError:
+            print("Poster - URLError. Try again.")
+        except timeout:
+            print("Poster - Connection timeout. Try again.")
+
+    def _movie_poster(self):
+        """
+        obs: tem um conserto no azak
+        """
         poster = self.soup.find(itemprop="image")
         re_poster = re.compile("http.*\.jpg")
         result = re_poster.search(str(poster))
         return result.group(0)
 
-    def synopsis(self):
-        description = self.soup.find(itemprop="description")
-        raw_txt = description.get_text()
-        return raw_txt
+    @staticmethod
+    def _save_poster_file(data):
+        img = QImage()  # (8,10,4)
+        img.loadFromData(data)
+        # TODO: save file in .cache/movie_plist - self.movie.title_year
+        img.save('/tmp/picture.png')
 
+    def _get_html(self, url):
+        """
+        obs: tem um conserto no azak
+        """
+        try:
+            return urllib.request.urlopen(url, timeout=3).read()
+        except urllib.error.URLError:
+            self.context = "HTML - URLError. Try again."
+        except timeout:
+            self.context = "HTML - Connection timeout. Try again."
+        except ValueError:
+            self.context = "HTML - Please, check the .desktop file for this movie."
+
+    # def parse_html(self):
+    #            self.soup = BeautifulSoup(self.html, 'html.parser')
--- html_file/htmltags.py
+++ html_file/htmltags.py
@@ -2,86 +2,86 @@
 """
    
 """
-
-import urllib.request
-import urllib.error
 import textwrap
-from socket import timeout
-from PyQt5.QtGui import QImage
+
 from data import pimdbdata
 
 
 class HtmlTags:
     def __init__(self, url):
-        self.url = url
+        self._url = url
         self.context = ''
-        self.synopsis = ''
-        self.html = ''
-        self.movie = ''
+        self._synopsis = ''
+        # self.html = ''
+        # self.movie = ''
 
-        self.retrieve_data()
-        self.build_html()
+        self._retrieve_data()
+        self._build_html()
 
-    def retrieve_data(self):
-        self.get_html()
-        self.parse_html()
-        self.get_synopsis()
-        self.wrap_synopsis()
-        self.do_poster_png_file()
+    def _retrieve_data(self):
+        # self.get_html()
+        # self.parse_html()
+        self._get_synopsis()
+        self._wrap_synopsis()
+        # self.do_poster_png_file()
 
-    def build_html(self):
-        self.top_header()
-        self.inside_table()
-        self.bottom_tags()
+    def _build_html(self):
+        self._top_header()
+        self._inside_table()
+        self._bottom_tags()
 
-    def get_html(self):
-        try:
-            self.html = urllib.request.urlopen(self.url, timeout=3).read()
-        except urllib.error.URLError:
-            self.context = "HTML - URLError. Try again."
-        except timeout:
-            self.context = "HTML - Connection timeout. Try again."
-        except ValueError:
-            self.context = "HTML - Please, check the .desktop file for this movie."
+    # def get_html(self):
+    #     try:
+    #         self.html = urllib.request.urlopen(self.url, timeout=3).read()
+    #     except urllib.error.URLError:
+    #         self.context = "HTML - URLError. Try again."
+    #     except timeout:
+    #         self.context = "HTML - Connection timeout. Try again."
+    #     except ValueError:
+    #         self.context = "HTML - Please, check the .desktop file for this movie."
 
-    def parse_html(self):
-        self.movie = pimdbdata.ParseImdbData(self.html)
+    # def parse_html(self):
+    #     self.movie = pimdbdata.ParseImdbData(self.html)
 
-    def get_synopsis(self):
-        self.synopsis = self.movie.synopsis()
+    def _get_synopsis(self):
+        html_movie = pimdbdata.ParseImdbData(self._url)
+        self._synopsis = html_movie.synopsis()
 
-    def wrap_synopsis(self):
-        self.synopsis = '<br />'.join(textwrap.wrap(self.synopsis))
+    def _wrap_synopsis(self):
+        self._synopsis = '<br />'.join(textwrap.wrap(self._synopsis))
 
-    def do_poster_png_file(self):
-        poster = self.movie.movie_poster()
-        try:
-            data = urllib.request.urlopen(poster).read()
-        except urllib.error.URLError:
-            self.context = "Poster - URLError. Try again."
-        except timeout:
-            self.context = "Poster - Connection timeout. Try again."
+    # def do_poster_png_file(self):
+    #     poster = self.movie.movie_poster()
+    #     try:
+    #         data = urllib.request.urlopen(poster).read()
+    #     except urllib.error.URLError:
+    #         self.context = "Poster - URLError. Try again."
+    #     except timeout:
+    #         self.context = "Poster - Connection timeout. Try again."
+    #
+    #     img = QImage()  # (8,10,4)
+    #     img.loadFromData(data)
+    #     # save file in .cache/movie_plist - self.movie.title_year
+    #     img.save('/tmp/picture.png')
 
-        img = QImage()  # (8,10,4)
-        img.loadFromData(data)
-        # save file in .cache/movie_plist - self.movie.title_year
-        img.save('/tmp/picture.png')
-
-    def top_header(self):
+    def _top_header(self):
         """ from <html> tag until <table> tag """
         self.context = ("<html><head>"
                         "<meta http-equiv=\"Content-Type\" content=\"text/html_file; charset=utf-8\">"
                         "</head><body>"
                         "<table>")
 
-    def inside_table(self):
+    def _inside_table(self):
+        """
+        '/tmp/picture.png' hardcoded - see pimdbdata.ParseImdbData
+        """
         self.context += "<td>\n<img src=\"/tmp/picture.png\"></td>"
         self.context += "<td width=\"70\">"
-        self.context += self.synopsis + "<br>"
-        self.context += "<a href=\"" + self.url + "\">imdb</a><br>"
+        self.context += self._synopsis + "<br>"
+        self.context += "<a href=\"" + self._url + "\">imdb</a><br>"
         self.context += "</td>"
 
-    def bottom_tags(self):
+    def _bottom_tags(self):
         """ from </table> to </html> """
         self.context += "</table><input type=submit value=\"Submit\"></form></body>\n</html>"
 
