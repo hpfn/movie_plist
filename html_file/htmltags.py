761aa75e6a2f4acc3e4e8778e8a3bb106f29d4a0
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-05-13 08:40:00 -0300

    Cleaning comments

--- html_file/htmltags.py
+++ html_file/htmltags.py
@@ -12,36 +12,18 @@ class HtmlTags:
         self._url = url
         self.context = ''
         self._synopsis = ''
-        # self.html = ''
-        # self.movie = ''
 
         self._retrieve_data()
         self._build_html()
 
     def _retrieve_data(self):
-        # self.get_html()
-        # self.parse_html()
         self._get_synopsis()
         self._wrap_synopsis()
-        # self.do_poster_png_file()
 
     def _build_html(self):
         self._top_header()
         self._inside_table()
         self._bottom_tags()
-
-    # def get_html(self):
-    #     try:
-    #         self.html = urllib.request.urlopen(self.url, timeout=3).read()
-    #     except urllib.error.URLError:
-    #         self.context = "HTML - URLError. Try again."
-    #     except timeout:
-    #         self.context = "HTML - Connection timeout. Try again."
-    #     except ValueError:
-    #         self.context = "HTML - Please, check the .desktop file for this movie."
-
-    # def parse_html(self):
-    #     self.movie = pimdbdata.ParseImdbData(self.html)
 
     def _get_synopsis(self):
         html_movie = pimdbdata.ParseImdbData(self._url)
@@ -50,26 +32,14 @@ class HtmlTags:
     def _wrap_synopsis(self):
         self._synopsis = '<br />'.join(textwrap.wrap(self._synopsis))
 
-    # def do_poster_png_file(self):
-    #     poster = self.movie.movie_poster()
-    #     try:
-    #         data = urllib.request.urlopen(poster).read()
-    #     except urllib.error.URLError:
-    #         self.context = "Poster - URLError. Try again."
-    #     except timeout:
-    #         self.context = "Poster - Connection timeout. Try again."
-    #
-    #     img = QImage()  # (8,10,4)
-    #     img.loadFromData(data)
-    #     # save file in .cache/movie_plist - self.movie.title_year
-    #     img.save('/tmp/picture.png')
-
     def _top_header(self):
         """ from <html> tag until <table> tag """
-        self.context = ("<html><head>"
-                        "<meta http-equiv=\"Content-Type\" content=\"text/html_file; charset=utf-8\">"
-                        "</head><body>"
-                        "<table>")
+        self.context = (
+            "<html><head>"
+            "<meta http-equiv=\"Content-Type\" content=\"text/html_file; charset=utf-8\">"
+            "</head><body>"
+            "<table>"
+        )
 
     def _inside_table(self):
         """
