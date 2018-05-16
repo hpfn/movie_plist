9860f601a328da6ffc58fd794d2888f1716639cc
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-05-16 18:15:59 -0300

    First step to test HtmlTags class

---
+++ tests/test_htmltags.py
@@ -0,0 +1,15 @@
+# import pytest
+# from pytest_mock import mocker
+# import mock
+import sys
+import os
+sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/.."))
+from html_file.htmltags import HtmlTags
+
+# @mock.patch('html_file.htmltags.HtmlTags')
+# def test_htmltags_class():
+#    assert 'Synopsis' in _html_page.context
+#    assert 'http://www.example.com' in _html_page.context
+
+
+
