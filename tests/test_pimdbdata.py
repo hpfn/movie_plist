6d46b8ea4601a7bd5adbf9b79ad6ffe1c37a35e1
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-05-17 17:27:34 -0300

    Two methods tested

---
+++ tests/test_pimdbdata.py
@@ -0,0 +1,24 @@
+import sys
+import os
+sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/.."))
+from data.pimdbdata import ParseImdbData
+
+
+# usar pytest.fixture ?
+
+def test_synopsis(mocker):
+    # mocker just mock methods, no attrs
+    #mocker.patch.object(ParseImdbData, 'BeautifulSoup', value=None)
+    mocker.patch.object(ParseImdbData, '__init__', return_value=None)
+
+    test_synopsis = ParseImdbData('http://www.example.com')
+    except_msg = test_synopsis.synopsis()
+    assert 'Maybe something is wrong' in except_msg
+
+
+def test_poster_url(mocker):
+    mocker.patch.object(ParseImdbData, '__init__', return_value=None)
+
+    test_poster = ParseImdbData()
+    poster_url = test_poster._poster_url()
+    assert 'https://static.significados.com.br/' in poster_url
