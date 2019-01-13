29d1d012d3d77f2e3ff3cbc523309643503d605a
hpfn <terberh@gmail.com>
2019-01-13 20:27:05 -0200

    Add test synopsis/cache_poster exists, reorganize file
    close #132

--- movie_plist/tests/test_pimdbdata.py
+++ movie_plist/tests/test_pimdbdata.py
@@ -9,14 +9,13 @@ from movie_plist.data.pimdbdata import ParseImdbData
 expected = [
 
     hasattr(pimdbdata, 'BeautifulSoup'),
-    # dunder init
-    # hasattr(pimdbdata.ParseImdbData, '_url'),
-    # hasattr(pimdbdata.ParseImdbData, 'title'),
-    # hasattr(pimdbdata.ParseImdbData, 'checked_description'),
     hasattr(pimdbdata, 'MOVIE_SEEN'),
     hasattr(pimdbdata, 'MOVIE_UNSEEN'),
     hasattr(pimdbdata, 'MOVIE_PLIST_CACHE'),
+    # inside init
     # hasattr(pimdbdata.ParseImdbData, 'synopsis'),
+    # hasattr(pimdbdata.ParseImdbData, 'cache_poster'),
+    # methods
     hasattr(pimdbdata.ParseImdbData, 'synopsis_exists'),
     hasattr(pimdbdata.ParseImdbData, 'bs4_synopsis'),
     hasattr(pimdbdata.ParseImdbData, 'add_synopsis'),
@@ -35,6 +34,9 @@ def test_init_mocked_attrs(e):
 
 @pytest.fixture()
 def init_mocked(mocker):
+    """
+    For whatever reason bs4 returns nothing
+    """
     mocker.patch.object(pimdbdata, 'MOVIE_PLIST_CACHE', return_value='home/.cache/movie_plist')
     mocker.patch.object(pimdbdata, 'BeautifulSoup', return_value=None)
     return ParseImdbData('url', 'title')
@@ -50,6 +52,9 @@ def test_poster_url(init_mocked):
 
 @pytest.fixture
 def run_init(mocker):
+    """
+    new movie scanned
+    """
     mocker.patch.object(ParseImdbData, '_poster_file',
                         return_value=b'tests/Shawshank_Redemption_1994.png')
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,73 +89,108 @@ def test_poster_name(run_init):
     assert file_name[-1] == 'Shawshank_Redemption_1994.png'
 
 
-@patch('movie_plist.data.pimdbdata.QImage')
-def test_do_save_poster_steps(img_mock, run_init, mocker):
-    mocker.patch.object(os.path, 'isfile', return_value=False)
-    run_init._do_poster_png_file()
-    assert img_mock.call_count == 1
-    img_mock.assert_has_calls(img_mock.loadFromData)
-    img_mock.assert_has_calls(img_mock.save)
-
-
-@patch('movie_plist.data.pimdbdata.QImage')
-def test_do_not_save_poster_steps(img_mock, run_init, mocker):
-    mocker.patch.object(os.path, 'isfile', return_value=True)
-    run_init._do_poster_png_file()
-    assert img_mock.call_count == 0
-    img_mock.loadFromData.assert_not_called()
-    img_mock.save.assert_not_called()
-
-
 @patch('movie_plist.data.pimdbdata.ParseImdbData._do_poster_png_file')
+@patch('movie_plist.data.pimdbdata.ParseImdbData.add_synopsis')
 @patch('movie_plist.data.pimdbdata.ParseImdbData.bs4_synopsis')
-def test_description_content(bs4_synopsis, do_poster_file, mocker):
+def test_description_content(bs4_synopsis, add_synopsis, do_poster_file, mocker):
+    """
+    What happens when synopsis does not exists
+    """
     mocker.patch.object(pimdbdata, 'BeautifulSoup', return_value=str())
     mocker.patch.object(ParseImdbData, 'synopsis_exists', return_value=False)
     obj = ParseImdbData('url', 'title')
-    # does not check what the method does
+    # does not check what the method does. only what it calls
     assert isinstance(obj.soup, str)
-    # do_poster_file.assert_called_once()
-    # bs4_synopsis.assert_called_once()
-    assert do_poster_file.call_count == 1
     assert bs4_synopsis.call_count == 1
-
-
-@patch('movie_plist.data.pimdbdata.ParseImdbData._do_poster_png_file')
-@patch('movie_plist.data.pimdbdata.ParseImdbData.bs4_synopsis')
-def test_synopsis_exists(bs4_synopsis, do_poster_file, mocker):
-    # does not check what the method does
-    mocker.patch.object(ParseImdbData, 'synopsis_exists', return_value=True)
-    obj = ParseImdbData('url', 'title')
-    assert do_poster_file.call_count == 0
-    assert bs4_synopsis.call_count == 0
-    assert not hasattr(obj, 'soup')
+    assert do_poster_file.call_count == 1
 
 
 @patch('movie_plist.data.pimdbdata.ParseImdbData.add_synopsis')
 def test_add_synopsis_attr(add, run_init):
+    """
+    When synopsis does not exists bs4_synopsis calls add_synopsis
+    """
     run_init.bs4_synopsis()
     assert add.call_count == 1
 
 
 @patch('movie_plist.data.pimdbdata.ParseImdbData.dict_movie_choice')
 def test_choice_no_made(choice, run_init):
+    """
+    json data does not have a record of the new movie
+    no choice can be made
+    the method works for old records
+    """
     run_init.add_synopsis()
-    # weird but a new movie goes to json file first then it will appear
-    # in a MOVIE* dict
-    # the method works for old records
     assert choice.call_count == 0
     assert run_init.title not in pimdbdata.MOVIE_UNSEEN
 
 
-def test_choice_unseen_made(run_init):
+def test_synopsis_exists():
+    """
+    Synopsis exists. Do nothing.
+    """
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    pimdbdata.MOVIE_PLIST_CACHE = os.path.join(base_dir, 'tests/.cache')
+    pimdbdata.MOVIE_UNSEEN = dict(
+        title=('/root', 'synopsis', 'any_path')
+    )
+
+    obj = ParseImdbData('url', 'title')
+    # soup attr only exists if there is no synopsis
+    assert not hasattr(obj, 'soup')
+    assert obj.synopsis == 'synopsis'
+    assert obj.cache_poster.endswith('movie_plist/tests/.cache/title.png')
+
+
+def test_choice_unseen(run_init):
+    """
+    A record exists and it goes to an unseen movie dict
+    """
     pimdbdata.MOVIE_UNSEEN[run_init.title] = ('root/',)
     run_init.bs4_synopsis()
     assert 'Directed' in pimdbdata.MOVIE_UNSEEN[run_init.title][1]
     del pimdbdata.MOVIE_UNSEEN[run_init.title]
 
 
-def test_choice_seen_made(run_init):
+def test_choice_seen(run_init):
+    """
+    A record exists and it goes to a seen movie dict
+    """
     pimdbdata.MOVIE_SEEN[run_init.title] = ('root/',)
     run_init.bs4_synopsis()
     assert 'Directed' in pimdbdata.MOVIE_SEEN[run_init.title][1]
+    del pimdbdata.MOVIE_SEEN[run_init.title]
+
+
+@patch('movie_plist.data.pimdbdata.ParseImdbData._save_poster_file')
+def test_do_save_poster_call(save_file, run_init, mocker):
+    """
+    There is no poster yet. Create one and save it
+    What is done when _save_poster_file is called
+    """
+    mocker.patch.object(os.path, 'isfile', return_value=False)
+    run_init._do_poster_png_file()
+    assert save_file.call_count == 1
+
+
+@patch('movie_plist.data.pimdbdata.QImage')
+def test_do_save_poster_steps(img_mock, run_init):
+    """
+    There is no poster yet. Create one and save it
+    What is done when _save_poster_file is called
+    """
+    run_init._save_poster_file()
+    assert img_mock.call_count == 1
+    img_mock.assert_has_calls(img_mock.loadFromData)
+    img_mock.assert_has_calls(img_mock.save)
+
+
+@patch('movie_plist.data.pimdbdata.ParseImdbData._save_poster_file')
+def test_do_not_save_poster_steps(save_file, run_init, mocker):
+    """
+    A poster exists. Do nothing.
+    """
+    mocker.patch.object(os.path, 'isfile', return_value=True)
+    run_init._do_poster_png_file()
+    assert save_file.call_count == 0
